2bb413a942c2e8c71569cd9f7c0e904d1ffb0d5c
renlf <renlfinfer@gmail.com>
2016-02-20 18:15:09 +0800

    Update md_cnn.py

--- md_cnn.py
+++ md_cnn.py
@@ -1,6 +1,10 @@
 import string
 import csv
 import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import  Convolution1D, MaxPooling1D
+from keras.optimizers import SGD
 
 char_array = [ch for ch in string.ascii_lowercase]
 num_array = [str(x) for x in range(0, 10)]
@@ -8,7 +12,9 @@ op_array = ['-']
 char_array = char_array + num_array + op_array
 
 char_dict = {}
-input_dim = 16
+shape_len = 16
+train_count = 1400
+test_count = 100
 
 
 def alexa_parser(filename):
@@ -31,9 +37,9 @@ def one_hot_encoder():
     return char_dict
 
 
-def md_encode(filename):
-    data = np.empty((500, 16, 37), dtype="float32")
-    label = np.empty((500,), dtype="float32")
+def md_encode(filename, data_count):
+    data = np.empty((data_count, shape_len, len(char_array)), dtype="float32")
+    label = np.empty((data_count,), dtype="float32")
 
     reader = open(filename, 'rb')
 
@@ -42,15 +48,19 @@ def md_encode(filename):
         d_name = domains[1]
         d_labl = domains[0]
         md_data = []
+
+        if len(d_name) > shape_len:
+            continue
+
         for ch in d_name:
             if char_dict.has_key(ch):
                 md_data.append(char_dict[ch])
             else:
                 md_data.append([0]*len(char_array))
-            print ch
-            print md_data
 
-        dim_remain = input_dim - len(d_name)
+#        print d_name
+
+        dim_remain = shape_len - len(d_name)
         while dim_remain > 0:
             md_data.append([0] * len(char_array))
             dim_remain -= 1
@@ -65,6 +75,37 @@ def md_encode(filename):
     return data, label
 
 
+def md_cnn():
+    (X_train, y_train) = md_encode('dn_train', train_count)
+    (X_test, y_test) = md_encode('dn_test', test_count)
+
+    model = Sequential()
+    model.add(Convolution1D(nb_filter=4, filter_length=4, border_mode='same', activation='tanh', subsample_length=1, input_shape=(shape_len, len(char_array))))#
+    model.add(Convolution1D(nb_filter=4, filter_length=2, border_mode='same', activation='tanh', subsample_length=1))
+    model.add(MaxPooling1D(pool_length=2))
+    model.add(Convolution1D(nb_filter=4, filter_length=2, border_mode='same', activation='tanh', subsample_length=1))
+    model.add(MaxPooling1D(pool_length=2))
+
+    model.add(Flatten())
+
+    model.add(Dense(256))
+    model.add(Dropout(0.25))
+    model.add(Activation('tanh'))
+
+    model.add(Dense(64))
+    model.add(Dropout(0.25))
+    model.add(Activation('tanh'))
+
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    sgd = SGD(l2=0.0, lr=0.05, decay=1e-2, momentum=0.9)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, class_mode='categorical')
+
+    model.fit(X_train,y_train, batch_size=128, nb_epoch=3, shuffle=True, show_accuracy=True, validation_split=0.2)
+
+
 #alexa_parser('top.csv')
 one_hot_encoder()
-md_encode('dn_train')
+#md_encode('dn_train', 500)
+md_cnn()
